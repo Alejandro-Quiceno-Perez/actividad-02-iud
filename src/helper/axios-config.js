c9eaa2f2f2dae506cb943b09b47d253d62381011
alejo <alejo.perez2905@gmail.com>
2024-10-13 21:35:41 -0500

    set production back url

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
+const baseUrl = {
+       local:"http://localhost:9000/api",
+       prod:"https://pelirredia-2.onrender.com/api"
+}
+
 const axiosInstance = axios.create({
-       baseURL: 'http://localhost:9000/api',
-       timeout: 1000,
+       baseURL: baseUrl.prod,
+       timeout: 10000,
        headers: { 'Content-Type': 'application/json' }
 })
 
